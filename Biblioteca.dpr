19d645995d0209fba80a0fd40fca7fdb9c8ba3b7
João Vitor Hiath <133877507+JoaoHiath@users.noreply.github.com>
2026-03-15 22:46:00 -0300

    Add files via upload

--- Biblioteca.dpr
+++ Biblioteca.dpr
@@ -2,10 +2,6 @@ program Biblioteca;
 
 uses
   Vcl.Forms,
-  TelaPrincipal in 'view\TelaPrincipal.pas' {frmTelaPrincipal},
-  ListaLivros in 'view\ListaLivros.pas' {frmListaLivros},
-  CadastroLivro in 'view\CadastroLivro.pas' {frmCadastroLivro},
-  CadastroCliente in 'view\CadastroCliente.pas' {frmCadastroCliente},
   Model.Livro in 'Model\Model.Livro.pas',
   Model.Cliente in 'Model\Model.Cliente.pas',
   Model.Emprestimo in 'Model\Model.Emprestimo.pas',
@@ -16,17 +12,18 @@ uses
   Repository.Livro in 'Repository\Repository.Livro.pas',
   Repository.Emprestimo in 'Repository\Repository.Emprestimo.pas',
   DataModulePrincipal in 'DataModule\DataModulePrincipal.pas' {dtmPrincipal: TDataModule},
-  ListaClientes in 'view\ListaClientes.pas' {frmListaClientes},
-  ListaEmprestimos in 'view\ListaEmprestimos.pas' {frmListaEmprestimos},
   Controller.Cliente in 'Controller\Controller.Cliente.pas',
   Factory.Cliente in 'Factory\Factory.Cliente.pas',
   Controller.Livro in 'Controller\Controller.Livro.pas',
   Factory.Livro in 'Factory\Factory.Livro.pas',
   Controller.Emprestimo in 'Controller\Controller.Emprestimo.pas',
   Factory.Emprestimo in 'Factory\Factory.Emprestimo.pas',
-  CadastroEmprestimo in 'view\CadastroEmprestimo.pas' {frmCadastroEmprestimo},
+  CadastrarLivro in 'view\CadastrarLivro.pas' {frmCadastrarLivro},
+  CadastrarLocacao in 'view\CadastrarLocacao.pas' {frmCadastrarLocacao},
+  CadastrarLocatario in 'view\CadastrarLocatario.pas' {frmCadastrarLocatario},
   DataDevolucao in 'view\DataDevolucao.pas' {frmDevolucao},
-  DataRenovacao in 'view\DataRenovacao.pas' {frmRenovacao};
+  DataRenovacao in 'view\DataRenovacao.pas' {frmRenovacao},
+  TelaPrincipalNew in 'view\TelaPrincipalNew.pas' {frmTelaPrincipalNew};
 
 {$R *.res}
 
@@ -34,6 +31,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TdtmPrincipal, dtmPrincipal);
-  Application.CreateForm(TfrmTelaPrincipal, frmTelaPrincipal);
+  Application.CreateForm(TfrmTelaPrincipalNew, frmTelaPrincipalNew);
   Application.Run;
 end.
